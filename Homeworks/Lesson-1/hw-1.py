dea9f34ca710908a26d89137e27529d2c9cf82f2
clever <edik22217@gmail.com>
2024-12-22 23:06:38 +0400

    l-1-hw-1-fix-1

--- Homeworks/Lesson-1/hw-1.py
+++ Homeworks/Lesson-1/hw-1.py
@@ -2,5 +2,7 @@ def pull(n: int) -> tuple[int, int]:
     max_epic = 10
     max_legendary = 90
     legendary = n // max_legendary
-    epic = n // max_epic if n % max_legendary != 0 else n // max_epic - 1
+    if n >= max_legendary:
+        n -= 1
+    epic = n // max_epic
     return epic, legendary
